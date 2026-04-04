1d115c8b1c7ce553a7b8110f4239734e50f2c177
ADMIRE-WT <dk488621@gmail.com>
2026-04-04 16:24:45 +0530

    change: updated name and added inorderTraversal

--- basics/tree/creationAndTraversal.cpp
+++ basics/tree/creationAndTraversal.cpp
@@ -2,7 +2,9 @@
 #include <vector>
 #include <iostream> 
 
-using namespace std;
+//for levelorder traversal 
+#include <queue>
+
 
 //main data structure
 class Tree{
@@ -15,7 +17,7 @@ class Tree{
 
 
 //function for tree creation
-Tree *preOrderToTree(vector<int> &preorderVector, int &index){
+Tree *preOrderToTree(std::vector<int> &preorderVector, int &index){
   if(index >= preorderVector.size() || preorderVector[index] == -1){    //if index is out of bound
     index++;
     return nullptr;                                                     //or is null return nullptr
@@ -32,7 +34,7 @@ Tree *preOrderToTree(vector<int> &preorderVector, int &index){
 
 
 //function for preordertriversal of the tree
-void preOrderTriversal(Tree *head){
+void preOrderTriversal(Tree* head){
   if(head == nullptr) return;
 
   std::cout<<head->data;
@@ -42,12 +44,48 @@ void preOrderTriversal(Tree *head){
 
 
 
+//function for levelOrder Traversel
+void levelOrderTraversal(std::queue<Tree*> head){
+  std::cout<<"\n";
+  
+  while(!head.empty()){
+    Tree* current = head.front();
+    head.pop();
+
+    if(current == nullptr){
+      if(head.empty()){
+        break;
+      }else{
+        head.push(nullptr);
+      }
+      std::cout<<"\n";
+    }
+    else{
+      std::cout<<current->data;
+      if(current->leftNode != nullptr){
+        head.push(current->leftNode);
+      }
+
+      if(current->rightNode != nullptr){
+        head.push(current->rightNode);
+      }
+    }
+  }
+}
+
+
 int main(){
-  vector<int> preorder = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+  std::vector<int> preorder = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
 
   int index = 0;
   Tree *head = preOrderToTree(preorder, index);
   preOrderTriversal(head);
+
+  std::queue<Tree*> levelOrder;
+  levelOrder.push(head);
+  levelOrder.push(nullptr);
+
+  levelOrderTraversal(levelOrder);
 
   return 0;
 }
